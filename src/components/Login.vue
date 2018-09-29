27b1383732f9223df157b06fa85f3f362524475a
WWW24362068 <af6_826887204_WWW24362068@git.cloud.tencent.com>
2018-09-29 20:29:45 +0800

    other.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,100 +1,52 @@
 <template>
   <div class="login">
-    <el-container>
-      <el-header>
-        <h3>Login in</h3>
-      </el-header>
-      <el-main>
-        <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px">
-          <el-form-item label="用户名" prop="username">
-            <el-input type="text" v-model="ruleForm2.username"></el-input>
-          </el-form-item>
-          <el-form-item label="密码" prop="pass">
-            <el-input type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="确认密码" prop="checkPass">
-            <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
-            <el-button @click="resetForm('ruleForm2')">重置</el-button>
-          </el-form-item>
-        </el-form>
-      </el-main>
-    </el-container>
+    <el-row :gutter="20">
+      <el-col :span="6"><div class="grid-content bg-purple">122</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">3353</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">444</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">555</div></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-      var checkUsername = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'));
-        }else {
-          callback();
-        }
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass');
-          }
-          callback();
-        }
-      };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm2.pass) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
-      return {
-        ruleForm2: {
-          pass: '',
-          checkPass: '',
-          username: ''
-        },
-        rules2: {
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          username: [
-            { validator: checkUsername, trigger: 'blur' }
-          ]
-        }
-      };
-    },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.row-bg {
-  align-items: center;
-  margin: 10px 0px;
-}
+    el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+    line-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
-import VueDirective from '@/examples/VueDirective'
-import Directive1 from '@/examples/VueDirecitive1'
-import Event from '@/examples/event'
-import Form from '@/examples/form'
+import Other from '@/examples/Other'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/HelloWorld',
+      path: '/',
       name: 'HelloWorld',
       component: HelloWorld
     },
@@ -22,24 +19,9 @@ export default new Router({
       component: Login
     },
     {
-      path: '/directive',
-      name: 'directive',
-      component: VueDirective
-    },
-    {
-      path: '/directive1',
-      name: 'directive1',
-      component: Directive1
-    },
-    {
-      path: '/event',
-      name: 'event',
-      component: Event
-    },
-    {
-      path: '/',
-      name: 'form',
-      component: Form
+      path: '/other',
+      name: 'Other',
+      component: Other
     }
   ]
 })
